1efa72afd37988ff17d95d1222f07927759129ed
Mayurmane1 <mayurhanumantmane@gmail.com>
2024-03-12 01:51:03 +0530

    Vendor product seller to shop useEffect

--- src/components/Modals/Vendors/AddProduct.jsx
+++ src/components/Modals/Vendors/AddProduct.jsx
@@ -361,8 +361,7 @@ const AddProduct = (props) => {
                 <button className='items-center p-1 bg-yellow-100 rounded-xl hover:bg-yellow-200' onClick={() => setOpen(true)}>
                     <Edit size={24} className='text-yellow-400' />
                 </button> :
-                <button className={`${formBtn1} flex`} onClick={() => setOpen(true)}>
-                    <Add className='text-white' />
+                <button className={`${formBtn1}`} onClick={() => setOpen(true)}>
                     {props?.title}
                 </button>}
             <Transition appear show={isOpen} as={Fragment}>
