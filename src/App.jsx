13334ec30d59d5814d29a71a6684d31d84162f00
shubuu <shubhamhandergule77@gmail.com>
2024-03-13 18:25:48 +0530

    firebase push notification

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,31 @@ import persistStore from "redux-persist/es/persistStore";
 import { PersistGate } from "redux-persist/integration/react";
 let persistor = persistStore(store);
 import { PrimeReactProvider } from 'primereact/api';
+import { getToken } from "firebase/messaging";
+import { useEffect } from "react";
+import { messaging } from "./firebase";
 
 
 const App = () => {
+  async function requestPermission() {
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      // Generate Token
+      const token = await getToken(messaging, {
+        vapidKey:
+          "BCNE5irFD6vNPpidF6AKkRjZ5KVq_g4M8HSAPGtRd9j9JGHgTVwQ085FCpW73Xp1rO_Dj0fUVhCpUtjC3mk0fT4",
+      });
+      console.log("Token Gen", token);
+      // Send this token  to server ( db)
+    } else if (permission === "denied") {
+      alert("You denied for the notification");
+    }
+  }
 
+  useEffect(() => {
+    // Req user for notification permission
+    requestPermission();
+  }, []);
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
